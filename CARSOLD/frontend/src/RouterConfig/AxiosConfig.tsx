d71a835d2580094469270c2c3ee2e196e9f07141
Gontar384 <gontarek384@gmail.com>
2024-11-02 22:19:54 +0100

    CARSOLD #4 | test commit

--- CARSOLD/frontend/src/RouterConfig/AxiosConfig.tsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;          // Enable sending cookies with requests
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
-
-export default axios;
