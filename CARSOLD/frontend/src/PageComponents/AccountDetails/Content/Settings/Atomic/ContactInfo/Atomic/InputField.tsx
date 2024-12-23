298ade2596e8842927868ea6049669fd0961fcb1
Gontar384 <gontarek384@gmail.com>
2024-12-23 12:14:12 +0100

    CARSOLD #57 | modified InputField layout

--- CARSOLD/frontend/src/PageComponents/AccountDetails/Content/Settings/Atomic/ContactInfo/Atomic/InputField.tsx
+++ CARSOLD/frontend/src/PageComponents/AccountDetails/Content/Settings/Atomic/ContactInfo/Atomic/InputField.tsx
@@ -71,29 +71,29 @@ const InputField: React.FC<InputFieldProps> = ({label, value, setValue, valueTyp
             <label className="text-xs xs:text-sm lg:text-base xl:text-lg 2xl:text-xl 3xl:test-2xl">
                 {label}
             </label>
-            <div className="flex flex-row w-fit text-sm xs:text-base lg:text-lg xl:text-xl
-            2xl:text-2xl 3xl:test-3xl cursor-pointer"
+            <div className="flex flex-row w-fit text-sm xs:text-base lg:text-lg xl:text-xl 2xl:text-2xl
+            3xl:test-3xl cursor-pointer"
                  onMouseEnter={!isMobile && buttonLabel === "Edit" ? () => setButtonActive(true) : undefined}
                  onMouseLeave={!isMobile && buttonLabel === "Edit" ? () => setButtonActive(false) : undefined}
                  onTouchEnd={isMobile ? () => setButtonActive(true) : undefined}
                  ref={componentRef}>
-                <div className="relative mr-5">
-                    <div className={`flex items-center w-[150px] xs:w-[180px] sm:w-[140px] lg:w-[180px] xl:w-[200px]
-                    2xl:w-[240px] 3xl:w-[270px] h-[21px] xs:h-[25px] lg:h-[29px] xl:h-[33px] 2xl:h-9 3xl:h-[38px]
-                    px-[2px] xs:px-[3px] lg:px-[4px] xl:px-[5px] 2xl:px-[6px] 3xl:px-[7px] rounded-sm overflow-hidden 
+                <div className="w-[150px] xs:w-[180px] sm:w-[140px] lg:w-[180px] xl:w-[200px] 2xl:w-[240px]
+                3xl:w-[270px] h-[21px] xs:h-[25px] lg:h-[29px] xl:h-[33px] 2xl:h-9 3xl:h-[38px] relative mr-5">
+                    <div className={`flex items-center w-full h-full px-[2px] xs:px-[3px] lg:px-[4px] xl:px-[5px]
+                    2xl:px-[6px] 3xl:px-[7px] rounded-sm overflow-hidden 
                     ${!inputActive ? "border border-black border-opacity-30" : ""}`}>
-                    {value}
+                        {value}
                     </div>
                     {inputActive &&
-                    <input className={`absolute inset-0 transition duration-300 ease-in-out focus:outline-none 
-                    focus:ring-2 xs:focus:ring-[3px] 2xl:focus:ring-4 rounded-sm focus:ring-blue-500/30
-                    focus:shadow-blue-500/50 px-[2px] xs:px-[3px] lg:px-[4px] xl:px-[5px] 2xl:px-[6px] 3xl:px-[7px]`}
-                           ref={inputRef}
-                           type="text" value={value}
-                           onChange={(e) => setValue(e.target.value.trim())}/>}
+                        <input className={`absolute inset-0 focus:outline-none rounded-sm px-[2px] xs:px-[3px]
+                        lg:px-[4px] xl:px-[5px] 2xl:px-[6px] 3xl:px-[7px]`}
+                               ref={inputRef}
+                               type="text" value={value}
+                               onChange={(e) => setValue(e.target.value.trim())}/>}
                 </div>
                 {buttonActive &&
-                    <button className={``}
+                    <button className={`w-10 xs:w-11 lg:w-[50px] xl:w-14 2xl:w-16 3xl:w-[70px] h-[21px] xs:h-[25px]
+                    lg:h-[29px] xl:h-[33px] 2xl:h-9 3xl:h-[38px] border border-black bg-lime rounded-sm`}
                             onClick={buttonLabel === "Edit" ? handleEditButtonClick : handleSaveButtonClick}>
                         {buttonLabel}
                     </button>
