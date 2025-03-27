beefc645082c69a3db173eca92d1bd2a6b942d7a
Gontar384 <gontarek384@gmail.com>
2025-03-27 16:46:44 +0100

    CARSOLD #126 | 1) Small update, going to work on laptop

--- CARSOLD/frontend/src/PageComponents/Home/Home.tsx
+++ CARSOLD/frontend/src/PageComponents/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LayOut from "../../LayOut/LayOut.tsx";
+import SearchFilters from "./SearchFilters/SearchFilters.tsx";
 
 const Home: React.FC = () => {
 
@@ -7,8 +8,8 @@ const Home: React.FC = () => {
 
     return (
         <LayOut>
-            <div className="flex flex-col">
-
+            <div className="flex flex-col items-center">
+                <SearchFilters/>
             </div>
         </LayOut>
     )
---
+++ CARSOLD/frontend/src/PageComponents/Home/SearchFilters/SearchFilters.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const SearchFilters: React.FC = () => {
+
+
+  return (
+      <div className="flex flex-col justify-center items-center w-full border-b border-black border-opacity-20">
+
+      </div>
+  )
+};
+
+export default SearchFilters
