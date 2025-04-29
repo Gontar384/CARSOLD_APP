80ceacf1c5f011027b191bb2cefeb126b7273485
Gontar384 <gontarek384@gmail.com>
2025-04-29 18:00:58 +0200

    CARSOLD #169 | 1) Modified Home Page layout

--- CARSOLD/frontend/src/PageComponents/Home/Home.tsx
+++ CARSOLD/frontend/src/PageComponents/Home/Home.tsx
@@ -5,7 +5,7 @@ import {FetchedOffer, UpdatedOffer} from "../AccountDetails/Content/MyOffers/MyO
 import SmallOfferDisplay from "../AccountDetails/Content/MyOffers/Atomic/SmallOfferDisplay.tsx";
 import HomeOfferLoading from "../../Additional/Loading/HomeOfferLoading.tsx";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faDoorOpen, faHouse, faMagnifyingGlass, faMoneyBillWave} from "@fortawesome/free-solid-svg-icons";
+import {faDoorOpen, faEarthAmericas, faMagnifyingGlass, faMoneyBillWave} from "@fortawesome/free-solid-svg-icons";
 import {Link} from "react-router-dom";
 import {useUtil} from "../../GlobalProviders/Util/useUtil.ts";
 
@@ -46,32 +46,31 @@ const Home: React.FC = () => {
 
     return (
         <LayOut>
-            <div className={`flex ${bigWidth ? "flex-row justify-between" : "flex-col items-center"} 
-            w-full h-full -mb-[500px] -mt-[60px] pt-14 pb-96 overflow-hidden relative bg-gradient-to-l from-lime to-darkLime`}>
-                <div className={`flex flex-col h-fit bg-gradient-to-t from-lime to-darkLime shadow-bottom overflow-hidden animate-appearFastRev
-                ${bigWidth ? "rounded-r-[50%] w-[55%] min-h-[720px] z-10" : "rounded w-[90%] items-center min-h-[550px] px-5"}`}>
-                    <p className={`${bigWidth ? "ml-16 mt-44" : "mt-24"} text-2xl m:text-3xl animate-appearLongRev`}>
+            <div className={`flex ${bigWidth ? "flex-row" : "flex-col items-center"} w-full h-full overflow-hidden relative`}>
+                <div className={`flex flex-col bg-gradient-to-t from-lime to-darkLime animate-appearFastRev
+                ${bigWidth ? "w-[50%] min-h-[720px] justify-center z-10" : "rounded w-[90%] items-center min-h-[500px] px-5"}`}>
+                    <p className={`${bigWidth ? "ml-20" : "mt-24"} text-2xl m:text-3xl animate-appearLongRev`}>
                         Welcome to CAR$OLD!</p>
-                    <div className={`flex flex-row items-center gap-2 ${bigWidth && "ml-16"} mt-6 animate-appearLongRev`}>
-                        <p className="text-xl m:text-2xl">Place, where all the best cars are</p>
-                        {bigWidth && <FontAwesomeIcon icon={faHouse} className="text-2xl m:text-3xl"/>}
+                    <div className={`flex flex-row items-center  gap-2 ${bigWidth && "ml-20"} mt-6 animate-appearLongRev`}>
+                        <p className="text-[20px] m:text-[26px]">A world of cars</p>
+                        <FontAwesomeIcon icon={faEarthAmericas} className="text-2xl m:text-3xl"/>
                     </div>
-                    <div className={`flex flex-row items-center gap-2 ${bigWidth && "ml-16"} mt-16 animate-appearLongRev`}>
+                    <div className={`flex flex-row items-center gap-2 ${bigWidth && "ml-20"} mt-10 animate-appearLongRev`}>
                         <Link to={"/authenticate/login"} className="text-xl m:text-2xl underline">Join us here</Link>
                         <FontAwesomeIcon icon={faDoorOpen} className="text-2xl m:text-3xl"/>
                     </div>
-                    <div className={`flex flex-row items-center gap-2 ${bigWidth && "ml-16"} mt-10 animate-appearLongRev`}>
+                    <div className={`flex flex-row items-center gap-2 ${bigWidth && "ml-20"} mt-10 animate-appearLongRev`}>
                         <Link to={"/search?page=0&size=10 "} className="text-xl m:text-2xl underline">Find your dream car</Link>
                         <FontAwesomeIcon icon={faMagnifyingGlass} className="text-2xl m:text-3xl"/>
                     </div>
-                    <div className={`flex flex-row items-center gap-2 ${bigWidth && "ml-16"} mt-10 animate-appearLongRev`}>
+                    <div className={`flex flex-row items-center gap-2 ${bigWidth && "ml-20"} mt-10 animate-appearLongRev`}>
                         <Link to={"/addingOffer"} className="text-xl m:text-2xl underline">Sell your car</Link>
                         <FontAwesomeIcon icon={faMoneyBillWave} className="text-2xl m:text-3xl"/>
                     </div>
                 </div>
-                <div className={`flex flex-col items-center justify-center h-fit bg-gradient-to-b from-lime to-darkLime shadow-bottom overflow-hidden
-                ${bigWidth ? "rounded-l-[50%] w-[55%] absolute right-0 min-h-[720px] z-20" : "rounded w-[90%] min-h-[600px] m:min-h-[860px] mt-5"} animate-appearFast`}>
-                    <div className={`flex flex-col justify-center ${bigWidth ? "w-full gap-8" : "w-[95%] gap-4 m:gap-6"}`}>
+                <div className={`flex flex-col items-center justify-center h-fit bg-gradient-to-t from-lime to-darkLime
+                ${bigWidth ? "w-[60%] absolute right-0 min-h-[720px] z-20" : "rounded w-[90%] min-h-[600px] m:min-h-[860px] mt-5"} animate-appearFast`}>
+                    <div className={`flex flex-col justify-center ${bigWidth ? "w-full gap-4" : "w-[95%] gap-4 m:gap-6"}`}>
                         <div className={`flex ${bigWidth ? "justify-end mr-5" : "justify-center"} -mt-8 m:-mt-10 animate-appearLong`}>
                             <div className={` ${bigWidth ? "max-w-[580px]" : "max-w-[670px]"} w-full`}>
                                 {fetched ? (offers[0] ? <SmallOfferDisplay type="search" offer={offers[0]} /> : null) : (<HomeOfferLoading />)}
