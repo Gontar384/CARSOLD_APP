4e3da88b0f5c1f7c1646a3731145a173278607b7
Gontar384 <gontarek384@gmail.com>
2025-06-13 17:18:45 +0200

    CARSOLD #233 | 1) Fixed IOS width bug in PasswordChange.tsx

--- CARSOLD/frontend/src/PageComponents/AccountDetails/Content/Settings/Atomic/PasswordChange/PasswordChange.tsx
+++ CARSOLD/frontend/src/PageComponents/AccountDetails/Content/Settings/Atomic/PasswordChange/PasswordChange.tsx
@@ -10,7 +10,7 @@ const PasswordChange: React.FC<PasswordChangeProps> = ({setIsChanged}) => {
     const {t} = useLanguage();
 
     return (
-        <div className="mt-36 m:mt-40">
+        <div className="mt-36 m:mt-40 w-full max-w-[270px] m:max-w-[340px]">
             <h2 className="mb-8 m:mb-10 text-xl m:text-2xl text-center">
                 {t("changePassword1")}
             </h2>
